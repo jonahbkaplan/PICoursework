ade156cf744bf6c9633e0df90eb48abb0e9f50ef
Jonah Kaplan <jonahkaplan@hotmail.com>
2024-03-28 14:47:36 +0000

    Revised the structure of PIData through the use of the new class PIDataType

--- Frontend/src/main/java/org/example/focus/PIData.java
+++ Frontend/src/main/java/org/example/focus/PIData.java
@@ -1,38 +1,8 @@
 package org.example.focus;
 
-import java.util.ArrayList;
-import java.util.List;
-
-//Abstract class for generic PIData to be extended for specific data
-public abstract class PIData<T> {
-
-    ArrayList<T> data = new ArrayList<>();
-    String name;
-
-    public PIData(String name){
-        this.name = name;
-    }
-
-    public String getName() { return  name; }
-
-    public void addItem(T item){
-        data.add(item);
-    }
-
-    public void removeItem(T item){
-        data.remove(item);
-    }
-
-    public List<T> getData(){
-        return data;
-    }
-
+public class PIData {
     @Override
     public String toString() {
-        StringBuilder output = new StringBuilder(STR."\{name}:\n");
-        for (T item : this.data){
-            output.append(item.toString());
-        }
-        return output.toString();
+        return super.toString();
     }
 }
