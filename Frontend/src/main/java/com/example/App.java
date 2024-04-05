c6d5b1fc406e785a9666c7485684cba5ce5ee724
MDBowen <morusdbowen@gmail.com>
2024-04-05 14:16:04 +0100

    switched to css files

--- Frontend/src/main/java/com/example/App.java
+++ Frontend/src/main/java/com/example/App.java
@@ -12,17 +12,16 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
-
-    String username = "Developer";
-    String password = "12345678";
 
     Scene scene;
     Scene scene2;
@@ -42,39 +41,33 @@ public class App extends Application {
         grid.setPadding(new Insets(0, 10, 0, 10));
 
         Label subtitle = new Label("Sign in");
-        subtitle.setTextFill(Color.WHITE);
         grid.add(subtitle, 0, 0);
 
         Label nameLabel = new Label("Username: ");
-        nameLabel.setTextFill(Color.WHITE);
         grid.add(nameLabel, 0, 1);
 
         Label passLabel = new Label("Password: ");
-        passLabel.setTextFill(Color.WHITE);
         grid.add(passLabel, 0, 2);
 
         TextField nameEntry = new TextField();
         nameEntry.setMaxWidth(150.0);
         nameEntry.setMaxHeight(8);
-        nameEntry.setStyle("-fx-background-color:darkgrey;");
         grid.add(nameEntry, 1, 1);
 
         TextField passEntry = new TextField();
         passEntry.setMaxWidth(150.0);
         passEntry.setMaxHeight(8);
-        passEntry.setStyle("-fx-background-color:darkgrey;");
         grid.add(passEntry, 1, 2);
 
         button = new Button("Enter");
         button.setOnAction(new EventHandler<ActionEvent>() {
-
             @Override
             public void handle(ActionEvent event) {
 
                 String givenUsername = nameEntry.getText().toString();
                 String givenPassword = passEntry.getText().toString();
 
-                if (username.equals(givenUsername) && password.equals(givenPassword)) {
+                if (checkdetails(givenUsername, givenPassword)) {
                     stage.setScene(scene2);
                 } else {
                     Alert alert = new Alert(AlertType.INFORMATION);
@@ -82,39 +75,57 @@ public class App extends Application {
                     alert.setHeaderText("Sign in details not found");
                     alert.setContentText("Please check your username and password");
                     alert.showAndWait();
-                    stage.setScene(scene2);
                 }
             }
         });
 
-        button.setStyle("-fx-background-color:darkgrey;");
-        grid.add(button, 3, 3);
+        grid.add(button, 1, 3);
 
         scene = new Scene(grid, 350, 200);
-
-        grid.setStyle("-fx-background-color:black;");
-        scene.setFill(Color.BLACK);
+        scene.getStylesheets()
+                .add("style1.css");
         stage.setScene(scene);
 
         // Layout 2
+
+        BorderPane border = new BorderPane();
+        HBox top = new HBox();
+        top.setPadding(new Insets(10));
+        // top.setStyle("-fx-background-color:black;");
+
+        // top.setBorder(new Border(new BorderStroke(null, BorderStrokeStyle.SOLID,
+        // null, null)));
+
+        border.setTop(top);
+
+        Label focus = new Label("focus");
+        focus.setFont(Font.font("Lucida Calligraphy", 40));
+        top.getChildren().addAll(focus);
 
         GridPane grid2 = new GridPane();
         grid2.setHgap(20);
         grid2.setVgap(20);
         grid2.setPadding(new Insets(0, 10, 0, 10));
 
+        border.setCenter(grid2);
+
         Label subtitle2 = new Label("Welcome");
-        subtitle2.setTextFill(Color.WHITE);
         grid2.add(subtitle2, 0, 0);
 
-        scene2 = new Scene(grid2, 1200, 700);
-        grid2.setStyle("-fx-background-color:black;");
-        scene2.setFill(Color.BLACK);
+        scene2 = new Scene(border, 1200, 700);
+        scene2.getStylesheets().add("style1.css");
+
         stage.show();
 
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public static boolean checkdetails(String username, String password) {
+        String correctUsername = "Developer";
+        String correctPassword = "12345678";
+        return (correctPassword.equals(password) && correctUsername.equals(username));
     }
 }
