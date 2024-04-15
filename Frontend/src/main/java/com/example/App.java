2654726045e68d4b7b860515143fd5d5a356dc3f
MDBowen <morusdbowen@gmail.com>
2024-04-15 15:58:26 +0100

    added metrics pane

--- Frontend/src/main/java/com/example/App.java
+++ Frontend/src/main/java/com/example/App.java
@@ -31,8 +31,6 @@ public class App extends Application {
 
     Boolean sidePanelOpen = false;
 
-    Button button;
-
     @Override
     public void start(Stage stage) throws IOException {
 
@@ -177,6 +175,18 @@ public class App extends Application {
             @Override
             public void handle(ActionEvent event) {
                 border.setCenter(session);
+            }
+        });
+
+        // Metrics window
+
+        MetricsPane metrics = new MetricsPane();
+        metrics.formatPane();
+
+        menugrid.metricsButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                border.setCenter(metrics);
             }
         });
 
