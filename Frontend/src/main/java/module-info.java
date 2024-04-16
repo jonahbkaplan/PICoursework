2f38be65cbb862b0da20bf0aa314b27fced5309b
MDBowen <morusdbowen@gmail.com>
2024-04-16 18:42:00 +0100

    fixed module info

--- Frontend/src/main/java/module-info.java
+++ Frontend/src/main/java/module-info.java
@@ -1,10 +1,15 @@
-
 module com.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires morphia.core;
+    requires org.mongodb.bson;
+    requires org.mongodb.driver.sync.client;
+    requires org.mongodb.driver.core;
 
     opens com.example to javafx.fxml;
-    exports com.example;
-}
+    opens com.example.morphia.entities to morphia.core;
 
+    exports com.example;
+    exports com.example.morphia.entities;
+}
