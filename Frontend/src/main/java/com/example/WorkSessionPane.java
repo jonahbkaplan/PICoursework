70f829f43981ef51c8a095ccab728c6cb5424972
MDBowen <morusdbowen@gmail.com>
2024-04-20 23:11:52 +0100

    Added table of tasks

--- Frontend/src/main/java/com/example/WorkSessionPane.java
+++ Frontend/src/main/java/com/example/WorkSessionPane.java
@@ -4,6 +4,11 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.morphia.entities.FocusSession;
+import com.example.morphia.entities.Task;
 
 import javafx.animation.AnimationTimer;
 import javafx.event.ActionEvent;
@@ -11,58 +16,151 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
-public class WorkSessionPane extends GridPane {
+public class WorkSessionPane extends BorderPane {
 
-    private LocalDateTime starttime;
-    private long minutes;
-    private Boolean inSession = false;
+    public List<Task> currenttasks = new ArrayList<Task>();
+    public VBox currentTaskList = new VBox();
+
+    public Boolean inSession = false;
+    public LocalDateTime currentStartTime;
+    public List<Task> allTasks = new ArrayList<Task>();
+
+    public ArrayList<FocusSession> recordedSessions = new ArrayList<FocusSession>();
 
     public void formatPane() {
 
-        this.setHgap(20);
-        this.setVgap(20);
-        this.setPadding(new Insets(0, 10, 0, 10));
+        this.setPadding(new Insets(100, 100, 100, 100));
+        allTasks.add(new Task(LocalDateTime.now(), LocalDateTime.now(), "Demo Task", "Done"));
+        allTasks.add(new Task(LocalDateTime.now(), LocalDateTime.now(), "Demo Task2", "None"));
+        BorderPane tasksPane = new BorderPane();
+        tasksPane.getStyleClass().add("work-gridpanes");
+        GridPane sessionPane = new GridPane();
+        sessionPane.getStyleClass().add("work-gridpanes");
+        formatTasksPane(tasksPane);
+        formatSessionPane(sessionPane);
+        this.setCenter(tasksPane);
+        this.setLeft(sessionPane);
+        this.getStyleClass().add("work-backpane");
 
-        this.add(new Label("Start Work Session"), 0, 0);
+    }
+
+    public void formatSessionPane(GridPane sessionPane) {
+
+        sessionPane.add(new Label("Start Session"), 0, 0);
 
         Button startButton = new Button("start");
-        this.add(startButton, 1, 0);
+        sessionPane.add(startButton, 0, 1);
         startButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-
                 if (!inSession) {
-                    starttime = LocalDateTime.now();
-                    minutes = starttime.getLong(ChronoField.MINUTE_OF_DAY);
                     startButton.setText("stop");
+                    currentStartTime = LocalDateTime.now();
                 } else {
                     startButton.setText("start");
+                    LocalDateTime endTime = LocalDateTime.now();
+                    if (currenttasks.isEmpty()) {
+                        currenttasks.add(new Task(currentStartTime, endTime, "undefined", "null"));
+                    }
+                    double timeElapsed = endTime
+                            .minus(currentStartTime.getLong(ChronoField.MINUTE_OF_DAY), ChronoUnit.MINUTES)
+                            .getLong(ChronoField.MINUTE_OF_DAY);
+                    recordedSessions.add(new FocusSession(currenttasks, timeElapsed)); // timeElapsed is number of
+                                                                                       // minutes
+
+                    currenttasks.clear();
+                    currentTaskList.getChildren().clear();
                 }
-
                 inSession = !inSession;
-
             }
+
         });
 
-        Label displayTimer = new Label("00:00");
-
+        Label displayTimer = new Label();
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
                 if (inSession) {
                     displayTimer.setText(
-                            LocalDateTime.now().minus(minutes, ChronoUnit.MINUTES)
+                            LocalDateTime.now()
+                                    .minus(currentStartTime.getLong(ChronoField.MINUTE_OF_DAY),
+                                            ChronoUnit.MINUTES)
                                     .format((DateTimeFormatter.ofPattern("HH:mm"))));
                 } else {
-                    displayTimer.setText("00:00");
+                    displayTimer.setText("");
                 }
             }
         };
         timer.start();
-        this.add(displayTimer, 1, 1);
+        sessionPane.add(displayTimer, 0, 2);
+
+        sessionPane.add(new Label("Currently doing:"), 0, 3);
+        sessionPane.add(currentTaskList, 0, 4);
 
     }
 
+    public void formatTasksPane(BorderPane tasksPane) {
+
+        GridPane grid = new GridPane();
+        tasksPane.setTop(grid);
+
+        grid.add(new Label("Manage Tasks"), 0, 0);
+
+        Button addTaskButton = new Button("Create new task");
+        grid.add(addTaskButton, 0, 1);
+        grid.add(new Label("Tasks To Do:"), 0, 2);
+
+        // Table
+
+        VBox Vtable = new VBox();
+        tasksPane.setCenter(Vtable);
+
+        GridPane tabletop = new GridPane();
+
+        String[] topRowStrings = { "Task                ",
+                "Start at            ",
+                "Finish by           ",
+                "Status              ",
+                "Manage " };
+
+        for (int i = 0; i < topRowStrings.length; i++) {
+            tabletop.add(new Label(topRowStrings[i]), i, 0);
+        }
+
+        for (Task task : allTasks) {
+            try {
+                TaskTableRow taskrow = new TaskTableRow(task);
+
+                Vtable.getChildren().add(taskrow);
+
+                taskrow.taskFinished.setOnAction(new EventHandler<ActionEvent>() { // Currently throws null pointer
+                                                                                   // exception
+                    @Override
+                    public void handle(ActionEvent event) {
+                        taskrow.setRowStatus("Finished");
+                        Vtable.getChildren().remove(taskrow);
+                    }
+                });
+
+                taskrow.doTask.setOnAction(new EventHandler<ActionEvent>() { // Currently throws null pointer
+                    // exception
+                    @Override
+                    public void handle(ActionEvent event) {
+                        taskrow.setRowStatus("In progress");
+                        task.setStatus("In progress");
+                        currenttasks.add(task);
+                        currentTaskList.getChildren().add(new Label(task.getDescription()));
+                    }
+                });
+
+            } catch (NullPointerException e) {
+                //
+            }
+
+        }
+    }
 }
