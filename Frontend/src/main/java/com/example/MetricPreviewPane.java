d56c15efe83477919f13a44d9a6b898e52be8563
GingerNinja8896 <jonahkaplan@hotmail.com>
2024-04-22 19:38:45 +0100

    More work on metrics tab

---
+++ Frontend/src/main/java/com/example/MetricPreviewPane.java
@@ -0,0 +1,56 @@
+package com.example;
+
+import com.example.morphia.entities.StressRecording;
+import com.example.morphia.entities.User;
+import javafx.scene.Group;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class MetricPreviewPane extends GridPane {
+
+    DBConnection db = new DBConnection();
+    List<Metric> metrics = new ArrayList<>();
+    TimeRange range = TimeRange.Day;
+    MetricsPane metricsPane;
+
+    public MetricPreviewPane(MetricsPane metricsPane){
+        CreateMetrics();
+        this.metricsPane = metricsPane;
+    }
+
+    public void onTimeChanged(TimeRange newRange){
+        this.range = newRange;
+        for(Metric metric : metrics){
+            metric.getChart().setTimeRange(range);
+            metric.getChart().refresh();
+        }
+
+    }
+
+    public void CreateMetrics(){
+        User user = new User();//TODO get current logged in user
+        user.setStressRecordings(new ArrayList<>());
+        List<StressRecording> stressRecordings = user.getStressRecordings();
+        TimeLineChart stressChart = new TimeLineChart("Stress","",range);
+        stressChart.addSeries("",stressRecordings.stream().map(StressRecording::getTimeTaken).collect(Collectors.toList()), stressRecordings.stream().map(StressRecording::getAverageStressMetric).collect(Collectors.toList()));
+        metrics.add(new Metric("Stress",stressChart,metricsPane));
+
+        GridPane pane = new GridPane();
+        for (int i = 0; i < metrics.size(); i++) {
+            GridPane preview = metrics.get(i).getPreview();
+            pane.add(preview,i,1);
+        }
+        getChildren().add(pane);
+
+    }
+
+
+
+
+}
